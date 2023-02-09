ee96f8a339408ac91a74e83aee0217d71696ff17
jaison080 <jaisondennis080@gmail.com>
2023-02-09 11:03:01 +0530

    Procedure COmpleted

---
+++ scripts/procedure1.sql
@@ -0,0 +1,19 @@
+DECLARE
+    A NUMBER;
+    B NUMBER;
+    C NUMBER;
+PROCEDURE FINDMIN(X IN NUMBER,Y IN NUMBER,Z OUT NUMBER) IS
+BEGIN
+    IF X>Y THEN
+        Z:=Y;
+    ELSE
+        Z:=X;
+    END IF;
+END;
+BEGIN
+    A:=23;
+    B:=45;
+    FINDMIN(A,B,C);
+    DBMS_OUTPUT.PUT_LINE('MINIMUM OF (23,45) : '||C);
+END;
+/
